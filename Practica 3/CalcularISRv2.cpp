8652a714a69c38db2a1691388d396c89c06238d8
Doikson <joseangelcortesbaillet@gmail.com>
2024-02-01 23:34:40 -0600

    ISR v2 Completado

--- Practica 3/CalcularISRv2.cpp
+++ Practica 3/CalcularISRv2.cpp
@@ -1,55 +1,66 @@
 #include <iostream>
 using namespace std;
 
-struct CalcularISR {
-    double limite_inferior;
+// Estructura
+struct TablaISR {
+    double limiteInferior;
+    double cuotaFija;
     double tasa;
-    double cuota_fija;
 };
 
+// Variables globales
+double salario, resultado;
+int posicion; // Guarda una posición del Array
+
+//Funciones
+void iniciarTabla(TablaISR* ISR);
+
 int main() {
-    double salario, resultado;
-    int posicion; // Guarda una posicion del Array
+    
+    //Se inicia la tabla y se llena
+    TablaISR ISR[11];
+    iniciarTabla(ISR);
 
-    CalcularISR ISR[11]= {
-        {0.01, 0.00, 1.92},
-        {578.53, 11.11, 6.40},
-        {4910.19, 288.33, 10.88},
-        {8629.21, 692.96, 16.00},
-        {10031.08, 917.26, 17.92},
-        {12009.95, 1271.87, 21.36},
-        {24222.32, 3880.44, 23.52},
-        {38177.70, 7162.74, 30.00},
-        {72887.51, 17575.69, 32.00},
-        {97183.34, 25350.35, 34.00},
-        {291550.01, 91435.02, 35.00}
-    };
-
-    cout << "Ingresa tu Salario: $";
+    cout << "Ingresa tu Salario: $"; 
     cin >> salario;
 
-    // Obtencion del Limite Inferior Correspondiente
+    // Obtención de la posicion del Limite Inferior Correspondiente
     for (int i = 0; i < 11; i++) {
-        if (salario >= ISR[i].limite_inferior) {
+        if (salario >= ISR[i].limiteInferior) {
             posicion = i;
         }
-    }
+    } 
 
-    // Se ajusta la posicion para que exista dentro de los limites del Array
-    if (posicion == 11) {
+    // Ajuste de la posición para que exista dentro de los límites del Array
+    if (posicion == 11){
         posicion = 10;
     }
-
-    double limite_inferior = ISR[posicion].limite_inferior;
+    
+    double limiteInferior = ISR[posicion].limiteInferior;
     double tasa = ISR[posicion].tasa;
-    double cuota_fija = ISR[posicion].cuota_fija;
+    double cuotaFija = ISR[posicion].cuotaFija;
 
-    cout << "El limite inferior es: $" << limite_inferior << endl;
+    cout << "El límite inferior es: $" << limiteInferior << endl;
     cout << "La tasa es: %" << tasa << endl;
-    cout << "La Cuota Fija es: $" << cuota_fija << endl;
+    cout << "La Cuota Fija es: $" << cuotaFija << endl;  
 
-    resultado = (salario-limite_inferior)-(((salario-limite_inferior)*tasa)/100) + cuota_fija;
-    cout << "El IRS estimado es: $"<< resultado << endl;
+    resultado = (salario - limiteInferior) - (((salario - limiteInferior) * tasa) / 100) + cuotaFija;
+    cout << "El IRS estimado es: $" << resultado << endl;
 
     return 0;
+}
+
+
+void iniciarTabla(TablaISR* ISR) {
+    ISR[0] = {0.01, 0.00, 1.92};
+    ISR[1] = {578.53, 11.11, 6.40};
+    ISR[2] = {4910.19, 288.33, 10.88};
+    ISR[3] = {8629.21, 692.96, 16.00};
+    ISR[4] = {10031.08, 917.26, 17.92};
+    ISR[5] = {12009.95, 1271.87, 21.36};
+    ISR[6] = {24222.32, 3880.44, 23.52};
+    ISR[7] = {38177.70, 7162.74, 30.00};
+    ISR[8] = {72887.51, 17575.69, 32.00};
+    ISR[9] = {97183.34, 25350.35, 34.00};
+    ISR[10] = {291550.01, 91435.02, 35.00};
 }
